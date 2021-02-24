d320a8c417c8685eb1f1c7af9954a070cbd6972f
Choppa(aka. Shnipov) <ovodenb@gmail.com>
2021-02-24 21:32:46 +0300

    ENDD

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,23 +7,36 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view){
-        display(1);
-        displayPrice(1*5);
+        displayPrice(quantity);
+        displayThank();
+    }
+    public void decrement(View view){
+        if (quantity > 0)
+            quantity--;
+        display(quantity);
+    }
+    public void increment(View view){
+        quantity++;
+        display(quantity);
     }
     private void display(int number){
-        TextView zCounter = (TextView) findViewById(R.id.z_counter);
-        zCounter.setText("" + number);
+        TextView qCounter = (TextView) findViewById(R.id.q_counter);
+        qCounter.setText("" + number);
     }
     private void displayPrice(int number){
-        TextView zCounter = (TextView) findViewById(R.id.pr_counter);
-        zCounter.setText(number + "$");
+        TextView prCounter = (TextView) findViewById(R.id.pr_counter);
+        prCounter.setText("Total: " +number*5 + "$");
+    }
+    private void displayThank(){
+        TextView thank = (TextView) findViewById(R.id.thank);
+        thank.setText("Thank!");
     }
 
 }
