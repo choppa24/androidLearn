4b8b48bcd341f47574ea8b31c96fed69c2face47
Choppa(aka. Shnipov) <ovodenb@gmail.com>
2021-02-26 12:22:44 +0300

    end of 7th lesson

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,26 +7,50 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int pointCounter = 0;
+    private int pointCounterTeamA = 0;
+    private int pointCounterTeamB = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    public void point3(View view){
-        pointCounter+=3;
-        display();
+    public void reset(View view){
+        pointCounterTeamA = 0;
+        pointCounterTeamB = 0;
+        displayTeamA();
+        displayTeamB();
     }
-    public void point2(View view){
-        pointCounter += 2;
-        display();
+    public void point3TeamA(View view){
+        pointCounterTeamA+=3;
+        displayTeamA();
     }
-    public void point1(View view){
-        pointCounter += 1;
-        display();
+    public void point2TeamA(View view){
+        pointCounterTeamA += 2;
+        displayTeamA();
     }
-    private void display(){
-        TextView points = (TextView) findViewById(R.id.point_counter);
-        points.setText(""+ pointCounter);
+    public void point1TeamA(View view){
+        pointCounterTeamA+= 1;
+        displayTeamA();
+    }
+    public void point3TeamB(View view){
+        pointCounterTeamB+=3;
+        displayTeamB();
+    }
+    public void point2TeamB(View view){
+        pointCounterTeamB += 2;
+        displayTeamB();
+    }
+    public void point1TeamB(View view){
+        pointCounterTeamB+= 1;
+        displayTeamB();
+    }
+    private void displayTeamA(){
+        TextView points = (TextView) findViewById(R.id.point_counter_teamA);
+        points.setText(""+ pointCounterTeamA);
+    }
+    private void displayTeamB(){
+        TextView points = (TextView) findViewById(R.id.point_counter_teamB);
+        points.setText(""+ pointCounterTeamB);
     }
 }
